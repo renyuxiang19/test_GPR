41bbc0209d2c439e2fc866f9f07129712908efb5
renyuxiang19 <84446838+renyuxiang19@users.noreply.github.com>
2022-05-12 15:59:11 +0900

    Create test_gpr.R
    initial commit

---
+++ test_gpr.R
@@ -0,0 +1,64 @@
+install.packages("tidyverse")
+library(tidyverse)
+
+# Training data
+s <- c(0.1, 0.3, 0.5, 0.7, 0.9)
+z <- c(6, 4, 4, 7, 6)
+z<- as.matrix(z)
+par(mar=c(5,4,1,1)+0.1)
+plot(s,z, 
+     ylim = c(0,8), pch=19)
+
+# Kernel functions
+sof <- 0.5
+sd <- 5
+
+kernel_g <- function(s1, s2){
+  k <- (sd^2) * exp(-pi* (abs(s1-s2)/sof)^2) 
+  return(k)
+}
+
+kernel_m <- function(s1, s2){
+  k <- (sd^2) * exp(-2* (abs(s1-s2)/sof))
+  return(k)
+}
+
+kernel_b <- function(s1, s2){
+  dif <- abs(s1-s2)
+  if (dif < sof) {
+    k <- (sd^2) * (1-(dif/sof)) 
+  }else{
+    k <- 0
+  }
+  return(k)
+}
+
+kernel_e <- function(s1,s2){
+  k <- exp(-0.5*abs(s1-s2)^2)
+  return(k)
+}
+
+# Calculate covariance matrices
+make_cov <- function(s1, s2){
+  len_s1 <- length(s1)
+  len_s2 <- length(s2)
+  cov_m <- matrix(nrow = len_s1, ncol = len_s2)
+  for (i in 1:len_s2) {
+    cov_m[,i] <- map_dbl(s1, function(x) kernel_m(s1=x, s2=s2[i]) )
+  }
+  return(cov_m)
+}
+
+# Predict
+test_s <- seq(0, 1, length= 40)
+
+k <- make_cov(s1=s, s2=s)
+k_star <- make_cov(s1=test_s, s2=s)
+ # Add noise 
+noise <- rnorm(length(k_star),mean=0,sd=1) %>% matrix(ncol = ncol(k_star), nrow = nrow(k_star))
+r <- cov(noise)
+
+z_predict <- k_star %*% solve(k+r) %*% z
+
+# Plot predicting
+lines(test_s,z_predict)
