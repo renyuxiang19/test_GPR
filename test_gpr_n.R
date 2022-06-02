f6aabfac5dcd2e504040b979d5599f18614061f1
renyuxiang19 <renyuxiang19@gmail.com>
2022-06-03 01:06:31 +0900

    Try to use MLE method to estimate parameters.
    The likelihood becomes infinity, because the determinant of K11 is 0.

--- test_gpr_n.R
+++ test_gpr_n.R
@@ -2,14 +2,18 @@
 # library(tidyverse)
 # library(rlang)
 library(MASS)
+library(optimx)
 source("functions.r")
 
 ## Parameters of GPR
 filename <- "kaminokoike_SWS.dat"
-kernel_fun <- "kernel_g"
-sof_h <- 10
-sof_v <- 6
-sd <- 2
+kernel_fun <- "kernel_m"
+sof_h_t <- 5
+sof_v_t <- 8
+sd_t <- 5
+sof_h_r <- 10
+sof_v_r <- 10
+sd_r <- 10
 nu <- 1.25
 noise <- TRUE
 
@@ -17,7 +21,10 @@ noise <- TRUE
 mesh_size_v <- 0.1
 
 ## read 2D data.
-n_sws <- read_MAIC(filename) |> mutate(y=0)
+n_sws <- read_MAIC(filename) |> 
+  mutate(y=0) |>
+  rowid_to_column("ID") |>
+  select(-ID)
 
 ## Plot raw data.
 raw_pic <- ggplot(data=n_sws)+
@@ -38,19 +45,46 @@ testing <- list(depth$x, depth$min_depth, depth$max_depth) |>
     }) |>
   mutate(y=0)
 
-## Calculate cov matrix..
-make_k11 <- function(noise = TRUE){
+## Calculate cov matrix.
+### Using log-likelihood to optimize parameters
+#### Observation vector.
+z <- n_sws$nsws |> matrix()
+#### Parameter vector.
+para <- c(sof_h_t, sof_v_t, sd_t, sof_h_r, sof_v_r, sd_r)
+#### K11 function.
+make_k11 <- function(para){
+  sof_h <- para[1]
+  sof_v <- para[2]
+  sd <- para[3]
   k11_h <- make_cov(m1 = n_sws[c("x","y")], m2 = n_sws[c("x","y")],
                     kernel = kernel_fun, sof = sof_h, sd = sd)
   k11_v <- make_cov(m1 = n_sws["z"], m2 = n_sws["z"],
                     kernel = kernel_fun, sof = sof_v, sd = sd)
   k11 <- k11_h * k11_v
-  if (noise){
-    r <- diag(nrow(k11))
-    k11 <- k11 + r
-  }
   return(k11)
 }
+#### likelihood function.
+ln_likelihood <- function(para){
+  sof_h_t <- para[1]
+  sof_v_t <- para[2]
+  sd_t <- para[3]
+  sof_h_r <- para[4]
+  sof_v_r <- para[5]
+  sd_r <- para[6]
+  #
+  m <- length(para)
+  k11_t <- make_k11(para[1:3])
+  k11_r <- make_k11(para[4:6])
+  f <- -0.5 * t(z) %*% ginv(k11) %*% z - 
+    0.5 * log(det(k11)) +
+    0.5 * m * log(2 * pi)
+  f <- -f
+  return(f)
+}
+#### Optimize
+ln_likelihood(para)
+a <- optimx(para, ln_likelihood, method = "BFGS")
+## Calculate k21
 make_k21 <- function(){
   k21_h <- make_cov(m1 = testing[c("x","y")], m2 = n_sws[c("x","y")],
                     kernel = kernel_fun, sof = sof_h, sd = sd)
@@ -60,7 +94,12 @@ make_k21 <- function(){
   return(k21)
 }
 k21 <- make_k21()
-k11 <- make_k11( noise = noise )
+
+#Add noise
+if (noise){
+  r <- diag(nrow(k11))
+  k11 <- k11 + r
+}
 
 ## Predict
 testing$nsws <- k21 %*% ginv(k11) %*% n_sws$nsws
